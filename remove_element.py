a51ffe35c9cd7653860ad80c060b7d73f452257e
Aniket kadukar <aniketdkadukar@gmail.com>
2024-05-05 13:55:50 +0530

    Created remove_element.py

---
+++ remove_element.py
@@ -0,0 +1,31 @@
+def removeElement(nums, val: int) -> int:
+    result = 0
+    start = 0
+    end = len(nums) - 1
+    
+    while start < end :
+        if nums[start] != val and nums[end] == val:
+            start += 1
+        
+        if nums[start] == val and nums[end] == val:
+            end -= 1
+        
+        if nums[start] != val and nums[end] != val:
+            start += 1
+        
+        if nums[start] == val and nums[end] != val:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1 
+    
+    for i in range(len(nums)):
+        if nums[i] != val:
+            result += 1
+    print(nums)
+    
+    return result
+
+
+nums = [4, 5]
+val = 5
+print(removeElement(nums, val))
